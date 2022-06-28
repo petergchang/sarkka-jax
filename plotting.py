066ee4f4f354f1277aa7e13d8a716781f1a602b8
petergchang <Peter0414@gmail.com>
2022-06-28 16:11:05 -0400

    Add plot_sine

--- plotting.py
+++ plotting.py
@@ -2,15 +2,7 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
 # Chapter 3
-def plot_linreg(X, Y_tr, Y, estimate=None):
-    """Plot linear regression example from Chapter 3.
-
-    Args:
-        X: time grid
-        Y_tr: "true" signal
-        Y: noisy measurements
-        estimate (optional): filtered estimates. Defaults to None.
-    """    
+def plot_linreg(X, Y_tr, Y, estimate=None):    
     plt.figure()
     plt.plot(X, Y, 'k.', label='Measurements')
     plt.plot(X, Y_tr, color='silver', linewidth=7, label="True Signal")
@@ -48,3 +40,17 @@ def plot_linreg_var_convergence(X, rvar1, rvar2, bvar1, bvar2):
     plt.gca().set_aspect(0.2)
     plt.legend(loc=1, borderpad=0.8, handlelength=4, fancybox=False, edgecolor='k');
     plt.grid(True, which="both", ls=':')
+
+def plot_sine(X, Y_tr, Y, estimate=None, xrlim=2., loc=1):
+    plt.figure()
+    plt.plot(X, Y, 'k.', label='Measurements')
+    plt.plot(X, Y_tr, color='silver', linewidth=7, label="True Signal")
+    if estimate is not None:
+        plt.plot(X, estimate, 'k-', linewidth=2, label="Estimate")
+    plt.xlabel('$t$'); plt.ylabel('$y$')
+    plt.xlim(0, 2); plt.ylim(-1.5, 1.5)
+    plt.xticks(jnp.arange(0, xrlim+0.1, 0.5))
+    plt.yticks(jnp.arange(-1.5, 1.6, 0.5))
+    plt.gca().set_aspect(0.5)
+    plt.legend(loc=loc, borderpad=0.8, handlelength=4, fancybox=False, edgecolor='k');
+    plt.show()
