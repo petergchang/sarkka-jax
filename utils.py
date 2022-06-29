f0a727ef8561e9020ad8915c9e88ec61ed57b834
petergchang <Peter0414@gmail.com>
2022-06-28 22:38:29 -0400

    Fix error in rmse computation

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import jax.numpy as jnp
 
 # Compute RMSE
 def compute_rmse(y, y_est):
-    return jnp.sqrt(jnp.sum(y-y_est)**2 / len(y))
+    return jnp.sqrt(jnp.sum((y-y_est)**2) / len(y))
 
+# Compute RMSE of estimate and print comparison with 
+# standard deviation of measurement noise
 def compute_and_print_rmse_comparison(y, y_est, R, est_type=""):
     rmse_est = compute_rmse(y, y_est)
     print(f'{f"The RMSE of the {est_type} estimate is":<40}: {rmse_est:.2f},')
